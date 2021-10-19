1a300b4a18f86f894bf4488890531e65e31adc92
Renan Rodrigues <renanrodriguesdossantos123@gmail.com>
2021-10-19 12:19:21 -0300

    Corrigindo alt images

--- src/project.js
+++ src/project.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Project = (props) =>{
     return (
-        <div className={props.index % 2 == 0?"row de-right-left bg-primary mt-1":"row de-left-right bg-primary mt-1"}>
+        <div className={props.index % 2 === 0?"row de-right-left bg-primary mt-1":"row de-left-right bg-primary mt-1"}>
             <h1 className="col-md-10 text-center pt-2">{props.project.name}</h1>
             <div className="col-md-5 p-3">
-                <img src={props.project.foto} className="foto"></img>
+                <img src={props.project.foto} alt="imagem do projeto" className="foto"></img>
             </div>
             <div className="col-md-5 font-4 text-center p-3">
                 {props.project.discription}
--- src/sobre.js
+++ src/sobre.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Sobre = () => {
     return (
         <div className="row" id="sobre-mim">
-            <div className="col-md-4"><img id="perfil" src="images/perfil.jpg"/></div>
+            <div className="col-md-4"><img id="perfil" alt="perfil" src="images/perfil.jpg"/></div>
             <div className="col-md-6 text-center">
                 <h1>Sobre Mim</h1>
                 <div id="div-sbm">
